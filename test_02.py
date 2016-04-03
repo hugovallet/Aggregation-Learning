eec95204b626ce091477b5e2ea8e4832c35353e5
ivoPe <ivaylo.petkantchin@gmail.com>
2016-04-03 12:54:49 +0200

    test 02 small changes_01

--- test_02.py
+++ test_02.py
@@ -14,13 +14,14 @@ Created on Tue Mar 29 18:54:45 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.close('all')
 
 #%% ARMA(1,1)
 
 r = 0.6
 teta = 0.2
-n_sample = 2000
-thres = 0.95
+n_sample = 1000
+thres = 0.99
 X = np.random.rand(n_sample)
 X[X<thres] = 0.0
 X[X>thres] = 1.0
@@ -37,25 +38,25 @@ plt.plot(Y)
 
 #%% ARMA(2,1)
 
-r1 = 0.5
-r2 = 0.3
-teta = 0.2
-n_sample = 1000
-thres = 0.95
-X = np.random.rand(n_sample)
-X[X<thres] = 0.0
-X[X>thres] = 5.0
-
-Y = []
-Y.append(X[0])
-Y.append(r1*Y[-1] - teta*X[0])
-for i in range(2,n_sample):
-    Y.append(r1*Y[-1] + r2*Y[-2] + X[i] - teta*X[i-1])
-Y = np.array(Y)
-
-plt.figure()
-plt.title("expl: ARMA(2,1)")
-plt.plot(Y)
+#r1 = 0.5
+#r2 = 0.3
+#teta = 0.2
+#n_sample = 1000
+#thres = 0.95
+#X = np.random.rand(n_sample)
+#X[X<thres] = 0.0
+#X[X>thres] = 5.0
+#
+#Y = []
+#Y.append(X[0])
+#Y.append(r1*Y[-1] - teta*X[0])
+#for i in range(2,n_sample):
+#    Y.append(r1*Y[-1] + r2*Y[-2] + X[i] - teta*X[i-1])
+#Y = np.array(Y)
+#
+#plt.figure()
+#plt.title("expl: ARMA(2,1)")
+#plt.plot(Y)
 
 #%% Partition for X
 
@@ -143,6 +144,8 @@ for n in range(10,n_sample):
 
 print "End"
 
+print "Precision: ", 1-np.mean(err)
+
 #%% Visualization
 
 plt.figure()
@@ -153,34 +156,34 @@ plt.legend(loc=0)
 
 #%% Test function
 
-n = 1382
-k = 2
-orde = 1
-
-X_tronc = X[:n]
-Y_tronc = Y[:n-1]
-Y_tronc2 = G(orde,Y_tronc)
-target = Y[n]
-
-n_sample = len(X_tronc)
-if(2*k<n_sample):
-    Y_tronc = G(orde,Y_tronc)
-    last_seq_Y = Y_tronc[-k+1:]
-    last_seq_X = X_tronc[-k:]
-    count = 0
-    val = 0
-    for i in range(n_sample-2*k):
-        if((list(X_tronc[i:i+k])==list(last_seq_X))and(list(Y_tronc[i:i+k-1])==list(last_seq_Y))):
-            count += 1.0
-            val += Y_tronc[i+k-1]
-            print Y_tronc[i+k-1]
-            #print val/count
-    if(count == 0):
-        out = 0.0
-    else:
-        out = val/count
-else:
-    out = 0.0
+#n = 1382
+#k = 2
+#orde = 1
+#
+#X_tronc = X[:n]
+#Y_tronc = Y[:n-1]
+#Y_tronc2 = G(orde,Y_tronc)
+#target = Y[n]
+#
+#n_sample = len(X_tronc)
+#if(2*k<n_sample):
+#    Y_tronc = G(orde,Y_tronc)
+#    last_seq_Y = Y_tronc[-k+1:]
+#    last_seq_X = X_tronc[-k:]
+#    count = 0
+#    val = 0
+#    for i in range(n_sample-2*k):
+#        if((list(X_tronc[i:i+k])==list(last_seq_X))and(list(Y_tronc[i:i+k-1])==list(last_seq_Y))):
+#            count += 1.0
+#            val += Y_tronc[i+k-1]
+#            print Y_tronc[i+k-1]
+#            #print val/count
+#    if(count == 0):
+#        out = 0.0
+#    else:
+#        out = val/count
+#else:
+#    out = 0.0
 
 
 
